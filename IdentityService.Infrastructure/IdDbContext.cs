c302d74b0b09658b1ef974830687d82a94cda181
eeee0717 <chentao020717Work@outlook.com>
2024-01-08 20:04:17 +0800

    chore: edit Program.cs

--- IdentityService.Infrastructure/IdDbContext.cs
+++ IdentityService.Infrastructure/IdDbContext.cs
@@ -6,13 +6,14 @@ namespace IdentityService.Infrastructure;
 
 public class IdDbContext : IdentityDbContext<User, Role, Guid>
 {
-    // public IdDbContext(DbContextOptions<IdDbContext> options)
-    //     : base(options)
-    // {
-    // }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=123456;database=IdentityService;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.2.0-mysql"));
-    
+    public IdDbContext(DbContextOptions<IdDbContext> options)
+        : base(options)
+    {
+    }
+    // 没有这行迁移会报错
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //     => optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=123456;database=IdentityService;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.2.0-mysql"));
+    //
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
--- IdentityService.WebAPI/Program.cs
+++ IdentityService.WebAPI/Program.cs
@@ -1,11 +1,41 @@
+using IdentityService.Domain;
+using IdentityService.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextFactory<IdDbContext>(options =>
+{
+  options.UseMySql("server=127.0.0.1;port=3306;user=root;password=123456;database=IdentityService;",
+    ServerVersion.Parse("8.2.0-mysql"));
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new() { Title = "IdentityService.WebAPI", Version = "v1" });
+  //c.AddAuthenticationHeader();
+});
+builder.Services.AddDataProtection();
+IdentityBuilder idBuilder = builder.Services.AddIdentityCore<User>(options =>
+{
+  options.Password.RequireDigit = false;
+  options.Password.RequireLowercase = false;
+  options.Password.RequireNonAlphanumeric = false;
+  options.Password.RequireUppercase = false;
+  options.Password.RequiredLength = 6;
+  //不能设定RequireUniqueEmail，否则不允许邮箱为空
+  //options.User.RequireUniqueEmail = true;
+  //以下两行，把GenerateEmailConfirmationTokenAsync验证码缩短
+  options.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultEmailProvider;
+  options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
+});
+idBuilder = new IdentityBuilder(idBuilder.UserType, typeof(Role), builder.Services);
+idBuilder.AddEntityFrameworkStores<IdDbContext>()
+  .AddDefaultTokenProviders()
+  .AddRoleManager<RoleManager<Role>>()
+  .AddUserManager<IdUserManager>();
 
 var app = builder.Build();
 
